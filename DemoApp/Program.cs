49d51ceca6ffe7b3c0b48e7192eaede82237e9d8
DenysGolub <holub.denys1122@vu.cdu.edu.ua>
2024-04-15 11:37:41 +0300

    added differention

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -5,70 +5,17 @@ namespace DemoApp
 {
     internal class Program
     {
+        static double F(double x)
+        {
+            return Math.Cos(x) + x * x;
+        }
         static void Main(string[] args)
         {
-            /*UnlinearEquations numerical_methods = new UnlinearEquations();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Differention differention = new Differention();
 
-           
-            numerical_methods.Function = "Cos(x)*Log(x+1,3)+1";
-
-            var chord = numerical_methods.Chord(2, 9);
-            var part_fraction = numerical_methods.PartFraction(2, 9);
-
-            Console.WriteLine("\tChord\t\t PartFraction");
-            for (int i = 0; i < chord.Count; i++)
-            {
-                Console.WriteLine($"{chord[i]}\t{part_fraction[i]}");
-            }*/
-
-            /*Console.WriteLine("Chord|Secant|Part_fraction|SimpleIter|newton");
-            for (int i = 0; i < chord.Count; i++)
-            {
-                Console.WriteLine($"{chord[i]}|{secant[i]}|{part_fraction[i]}|" +
-                    $"{iter[i]}|{newton[i]}");
-            }*/
-
-            /*Zeidel zeidel = new Zeidel();
-            double[,] numbers = { 
-                { 2,1,5,17}, 
-                {3,4.5,4,25}, 
-                {4,2,1,20}, 
-                };
-            var m = zeidel.Solve(numbers);*/
-
-            SimpleIteration si = new SimpleIteration();
-
-            double[,] system = new double[,]
-            {
-                { 2.66, 0.9, 15.3, 7.1 },
-                { 1.3, 22.8, 3.3, 6.89 },
-                { 15.8, -4.1, -1.7, 2.8 }
-            };
-            si.Epsilon = 0.1;
-
-            
-            //var solutions = si.Solve(system);
-
-            LU lu = new LU();
-            var b = lu.Solve(system);
-
-            var decompos = LU.Decompose(LinearSystem.GetMatrixWithoutVector(system));
-            ///AX=B => LUX=B
-            ///LY=B => Y
-            ///UX=Y => X
-            Zeidel zeidel = new Zeidel();
-            zeidel.Epsilon = 0.00000000000001;
-            var Y = zeidel.Solve(LinearSystem.MergeToSLAR(decompos.L, LinearSystem.GetVectorB(system)));
-            //var X = zeidel.Solve(zeidel.MergeToSLAR(decompos.U, Y.ToArray()));
-
-            double[,] array_b = new double[Y.Count, 1];
-
-            for(int i = Y.Count-1, x=0; i>=0; i--, x++)
-            {
-                array_b[x, 0] = Y.ElementAt(i).Value;
-            }
-            var X = zeidel.Solve(LinearSystem.MergeToSLAR(decompos.U, array_b));
-
+            double number = 5.12;
+            Console.WriteLine($"Похідна у точці '{number}': {differention.FirstDiff(F, number)}");
         }
     }
 }
---
+++ NumericalMethods/Differention.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods
+{
+    public class Differention
+    {
+        public double FirstDiff(Func<double, double> func, double x)
+        {
+            return (func(x+0.0001)-func(x))/0.0001;
+        }
+
+        public double FirstDiffX(Func<double, double, double> func, double x, double y)
+        {
+            return (func(x + 0.0001, y) - func(x, y)) / 0.0001;
+        }
+
+        public double FirstDiffY(Func<double, double, double> func, double x, double y)
+        {
+            return (func(x,y+0.0001) - func(x, y)) / 0.0001;
+        }
+
+        public double SecondDiff(Func<double, double> func, double x, double h=10)
+        {
+            return (func(x + h) - 2 * func(x) + func(1 - h)) / Math.Pow(h, 2);
+        }
+    }
+}
